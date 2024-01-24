aa6baf2e3c850702f44118413bda25d08462b812
Simon Ly <50601011+SiLyGoose@users.noreply.github.com>
2024-01-23 23:40:22 -0800

    Added initial valentine finder screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "@/app/page.module.css";
 
 import clsx from "clsx";
@@ -9,8 +9,29 @@ import NavBar from "@/app/components/NavBar";
 import Header from "@/app/components/Header";
 
 export default function Home() {
+	const [initialized, setInitialized] = useState(false);
+
 	return (
 		<main className={clsx("d-flex flex-column h-100 justify-content-between", styles.mainContainer)}>
+			<div
+				id="valentineFinder"
+				onClick={() => {
+					setInitialized(true);
+					setTimeout(() => {
+						document.getElementById("valentineFinder").style.display = "none";
+					}, 850);
+				}}
+				className={clsx(styles.popup, { [styles.hide]: initialized })}
+			>
+				<div className={styles.popupMask} />
+				<div className={styles.popupContainer}>
+					<div className={styles.prompt}>
+						<span>
+							Find your <span style={{ color: "var(--baby-pink)" }}>Valentine</span> today!
+						</span>
+					</div>
+				</div>
+			</div>
 			<Header />
 			<StarchCard />
 			<NavBar />
